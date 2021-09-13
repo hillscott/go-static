7e8318c25a024108f6e5cb7a29cda9405d844825
Ian Reynolds <hillscott@users.noreply.github.com>
2021-09-13 08:37:31 -0400

    Integrated improvement by shawnbrown
    Addresses file-systems that can't handle the chars generated by unquote in a programmatic way. The script will throw proper errors now.

--- urldecode-fs.py
+++ urldecode-fs.py
@@ -5,12 +5,15 @@
 # when using Simply Static on non-ASCII character URLs within Wordpress
 # Once located, the script will use urllib to translate the %-encoded name to UTF characters.
 # Ensure your file-system supports non-ASCII file-names!
+# unquote will throw an error if it doesn't
 # NOTE: If your site also has non-ASCII file-names, this script will need edits to function.
 # GPLv2 Licensing
 from urllib.parse import unquote
 import os
 import shutil
+import sys
 osdirLoc = "/INSERT-FS-PATH-HERE/"
+fs_encoding = sys.getfilesystemencoding()
 
 if ("/INSERT-FS-PATH-HERE/" in osdirLoc):
   print("Please set the ospath variable to continue...")
@@ -27,7 +30,8 @@ for root, dirs, files in os.walk(osdirLoc, topdown=False):
            print("DIR Encoding issue found!")
            path = os.path.join(root, name)
            print(path)
-           value = unquote(path)
+           # Fail out if the path can not be encoded by the fs
+           value = unquote(path, fs_encoding, 'strict')
            print(value)
            if (os.path.isdir(value)):
                print("Translated dir already exists...")
